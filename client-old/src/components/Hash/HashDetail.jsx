d34f6e54061cfb58f394a2afb63c379a8e3a3859
ErSukanya Chauhan <151927764+ErSukanyaChauhan@users.noreply.github.com>
2024-11-09 07:09:26 +0530

    Update HashDetail.jsx

--- client-old/src/components/Hash/HashDetail.jsx
+++ client-old/src/components/Hash/HashDetail.jsx
@@ -91,7 +91,7 @@ const HashDetail = (props, { theme, setTheme }) => {
 
         try {
             setloading(true);
-            const response = await fetch(`http://infosectool.com/api/hash/${hash}`);
+            const response = await fetch(`http://infosectool.com:4000/api/hash/${hash}`);
             const result = await response.json();
             console.log(result);
             setloading(false);
