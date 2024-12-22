6c42ad663d99766c5168f8aafc7943165151a4eb
ErSukanya Chauhan <151927764+ErSukanyaChauhan@users.noreply.github.com>
2024-12-22 16:05:25 +0530

    Update App.jsx

--- client-old/src/App.jsx
+++ client-old/src/App.jsx
@@ -4,6 +4,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HashDetail from './components/Hash/HashDetail';
 import Ipdetail from './components/ip/Ipdetail';
 import IpDetails from './components/ip/IpDetails';
+import AboutPage from './components/About/AboutPage';
+import Disclaimer from './components/Disclaimer/Disclaimer';
+import ContactUs from './components/Contact/ContactUs';
+import SocialTool from './components/SocialTool';
 const App = () => {
 
   return (
@@ -14,6 +18,10 @@ const App = () => {
           <Route element={<HashDetail />} path="/corresponding-file-hash-checker" />
           <Route element={<Ipdetail />} path="/ip-reputation-checker-on-multiple-TI" />
           <Route element={<IpDetails />} path="/ipdetails" />
+          <Route element={<AboutPage />} path="/about-us" />
+          <Route element={<Disclaimer />} path="/disclaimer" />
+          <Route element={<ContactUs />} path="/contact-us" />
+          <Route element={<SocialTool />} path="/social-tool" />
         </Routes>
       </BrowserRouter>
     </>
